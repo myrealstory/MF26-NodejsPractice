f663509fef7785b9a81b13eee36881672be59bf1
myrealstory <104052973+myrealstory@users.noreply.github.com>
2022-06-23 16:41:51 +0800

    20220623 updated
    updated nodejs
    today teaching about :
    -work like php include all parts
    -building server in video
    -more clear in linking

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.set('view engine','ejs');
+
 app.use(express.static("public"));
 app.use('/bootstrap/',express.static("node_modules/bootstrap/dist"));
 
 app.get('/',(req,res)=>{
-    res.send(`<h2>Hello Word</h2>`);
+    res.render('main',{ name: "hello World" });
     
 });
 
